f2b5e89bedda235ec9d9f7423e470182dbfe0521
rishu.kumar <rishu.kumar@peoplestrong.com>
2025-09-30 14:33:38 +0530

    change updates

--- task_mang/api.py
+++ task_mang/api.py
@@ -1,6 +1,8 @@
 from ninja import NinjaAPI
-from app.apis.api import api  
+from app.apis.api import api 
+from app.apis.auth_api import auth_api 
 
 app = NinjaAPI()
 
 app.add_router('v1/', api)
+app.add_router('v1',auth_api)
