8c16112329bd1b5fae9b31379c40c26eed614df0
rishu.kumar <rishu.kumar@peoplestrong.com>
2025-09-30 16:57:23 +0530

    change updates

--- app/apis/auth_api.py
+++ app/apis/auth_api.py
@@ -1,16 +1,10 @@
-from ninja import Router,Schema
+from ninja import Router,Schema,Form
 from ninja.security import HttpBearer, APIKeyHeader
 from app.models import UserProfile
 from django.contrib.auth import authenticate
 from jwtunits.custom_jwt import create_jwt_token
+from django.db import IntegrityError    
 
-class TokenAuth(HttpBearer):
-    def authenticate(self, request, token):
-        user = UserProfile.objects.filter(email=token).first()
-        if user:
-            return user
-        return None 
-    
 auth_api = Router(tags=["Authentication"])
 
 @auth_api.post('login/')
@@ -31,27 +25,32 @@ def logout(request):
 
 class RegisterSchema(Schema):
     email: str
-    password: str
     full_name: str = None
     phone_no: str = None
     pass1:str
     pass2:str 
 
-@auth_api.post("register/")
-def register(request, payload: RegisterSchema):
-    user = UserProfile.objects.create_user(
-        email=payload.email,
-        full_name=payload.full_name,
-        phone_no=payload.phone_no
-    )
+@auth_api.post("signup/")
+def signup(request, payload: Form[RegisterSchema]):
+    email = payload.email.strip().lower()
 
-    if payload.pass1.check_password(payload.pass2):
-        user.set_password(payload.pass1)
-        user.save()
+    if UserProfile.objects.filter(email__iexact=email).exists():
+        return {"status": False, "message": "This email id already exists!", "data": ""}
 
-    token = create_jwt_token(user.id)
-    return {
-        "status": True,
-        "message": "User registered successfully!",
-        "data": token
-    }
+    try:
+        user = UserProfile.objects.create_user(
+            email=email,
+            full_name=payload.full_name,
+            phone_no=payload.phone_no
+        )
+
+        if payload.pass1 == payload.pass2:
+            user.set_password(payload.pass1)
+            user.save()
+
+        token = create_jwt_token(user.id)
+        return {"status": True, "message": "User registered successfully!", "data": token}
+
+    except IntegrityError as e:
+        print("exption:- ",e)
+        return {"status": False, "message": f"{e}", "data": ""}
--- app/models.py
+++ app/models.py
@@ -1,32 +1,38 @@
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,PermissionsMixin
 
 
 ## custom user profile manager
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('The Email must be set')
+            raise ValueError("The Email must be set")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
         return self.create_user(email, password, **extra_fields)
-    
+
 
 ### custom user profile 
-class UserProfile(AbstractUser):
+class UserProfile(AbstractUser,PermissionsMixin):
     full_name=models.CharField(max_length=150,null=True,blank=True)
     email=models.EmailField(unique=True)
     phone_no = models.CharField(max_length=14, blank=True, null=True)
 
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
     objects=CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name","phone_no"]  
 
     created_at=models.DateTimeField(auto_now=True)
     updated_at=models.DateTimeField(auto_now_add=True)
